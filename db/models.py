1335fa5ddcf9e033d146275868007215b82bfa2a
Kuzma <kuzma.insarov813@gmail.com>
2025-07-09 16:18:39 +0300

    Solution2

--- db/models.py
+++ db/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import ForeignKey
 
 
 class Race(models.Model):
@@ -10,7 +9,7 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
 class Guild(models.Model):
@@ -22,7 +21,7 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey("Guild", on_delete=models.SET_NULL,
                               null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
